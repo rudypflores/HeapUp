35fc59ed6663560e87c10a27c86a7d8aefe9c420
Hoaian <hoaianguyen7@gmail.com>
2019-10-19 17:46:26 -0500

    Sdf

--- heapi/route/profile.js
+++ heapi/route/profile.js
@@ -1,8 +1,26 @@
 const express = require("express")
 const profile = express.Router();
 
-profile.get("/", (req, res) => {
-    res.send("hellow")
+profile.get("/:name", (req, res) => {
+    
+    //From Bo
+    var json = {gender : 'male', age : 3, weight : 453, bench : 234, 
+                squat : 789, deadlift : 987};
+
+    var name = req.params.name
+    var option = req.query.option
+    console.log(option)
+
+    var str = option.split(",")
+    console.log(str)
+
+    var j={};
+
+    for(var i = 0; i < str.length; i++) {
+        j[str[i]] = json[str[i]]
+    }
+
+    res.send(j)
 })
 
 module.exports = profile
