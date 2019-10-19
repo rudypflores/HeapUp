9659127bd34beb584d2d9b9dc790be7345ab8d19
rudy <rudypichola@gmail.com>
2019-10-19 15:37:16 -0500

    completed version of index.html

--- heap-up/src/components/Index/Index.js
+++ heap-up/src/components/Index/Index.js
@@ -3,12 +3,19 @@ import './index.css';
 
 
 const Index = () => {
-
     return (
         <div id="index">
+
+            <h1 id="instructions">Answer the following questions to begin:</h1>
             <form id="index-form">
-                <input id="name" type="text" placeholder="name"/>
-                <input id="weight" type="number" min="1" placeholder="weight"/>
+                <input id="name" className="text-input" type="text" placeholder="Name" required/>
+                <input id="gender" className="text-input" type="text" placeholder="Gender" required/>
+                <input id="age" className="num-input" type="number" placeholder="Age" required/>
+                <input id="weight" className="num-input" type="number" min="1" placeholder="Weight (lbs)" required/>
+                <input id="bench" className="num-input" type="number" min="1" placeholder="Bench: One rep max (lbs)"/>
+                <input id="squat" className="num-input" type="number" min="1" placeholder="Squat: One rep max (lbs)"/>
+                <input id="deadlift" className="num-input" type="number" min="1" placeholder="Dead Lift: One rep max (lbs)"/>
+
                 <input id="submit" type="submit"/>
             </form>
         </div>
