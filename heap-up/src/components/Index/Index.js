3e3699168840228e03e41c6b7de9bea7a5acca4c
rudy <rudypichola@gmail.com>
2019-10-19 13:59:16 -0500

    creation of homepage and navbar

---
+++ heap-up/src/components/Index/Index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css';
+
+
+const Index = () => {
+
+    return (
+        <div id="index">
+            <form id="index-form">
+                <input type="text" placeholder="name"/>
+                
+                <input type="submit"/>
+
+            </form>
+        </div>
+    );
+};
+
+export default Index;
---
+++ heap-up/src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return(
+        <div id="navbar">
+            <p id="title">Heap
+            <span id="title-x">Up</span>
+            <span id="title-y">!</span>
+            </p>
+        </div>
+    );
+};
+
+export default Navbar;
