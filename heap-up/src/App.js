fa863446441e4d2b2a4a5ffb518dac6fc51b2dd1
rudy <rudypichola@gmail.com>
2019-10-20 03:07:30 -0500

    fix conflict

--- heap-up/src/App.js
+++ heap-up/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home/Home';
-import Leaderboard from './components/Leaderboard/Leaderboard';
 
 const App = () => {
 
   return (
     <React.Fragment>
-        {/* <Leaderboard /> */}
         <Home />
     </React.Fragment>
   );
---
+++ heap-up/src/components/SignIn/SignIn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SignIn.css';
+
+
+const SignIn = () => {
+
+    return (
+        <React.Fragment>
+            <p>Sign In</p>
+        </React.Fragment>
+    );
+};
+
+export default SignIn;
