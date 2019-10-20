227a2d37f12c78b3390858ffa54bd5af0d0f5594
quoctru99 <trunguyen@Trus-MacBook-Pro.local>
2019-10-20 09:58:41 -0500

    finallll

--- heapi/route/authenthicate.js
+++ heapi/route/authenthicate.js
@@ -1,12 +1,22 @@
 const express = require("express")
+const User = require("../mongoose/user")
 
-module.exports = function isAuthenticated(req, res, next) {
-    var json = {"username" : "an", "password" : "an123"}
-    if (json.username === "an" && json.password === "an123") {
-        return next();
+module.exports = async function (req, res, next) {
+    const username = req.body.username
+    let password
+    try {
+        var user = await User.findOne({username: username})
+        password = user.password
+    } catch (e) {
+        return res.status(404).send("wrong username")
     }
-    else {
-        res.send({"error": "Authen Failed"});
+    if (!username || !password) {
+        return res.status(404).send("fail to authenticate 1")
     }
+    else if (password == req.body.password) {
+        return next()
+    }
+    
+    res.status(404).send("failed to authenticate")
 
 }
