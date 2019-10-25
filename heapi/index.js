1ecd0380a6e5160000855c9b2bfbcd433d9ed071
root <root@instance-1.us-central1-a.c.sturdy-willow-256501.internal>
2019-10-25 06:50:41 +0000

    last finaly commit

--- heapi/index.js
+++ heapi/index.js
@@ -7,18 +7,20 @@ const profile = require("./route/profile")
 const { mongodb } = require("./config/key")
 
 const app = express()
-const PORT = 8080 
+const PORT = process.env.PORT || 8080
 
 mongoose.connect(mongodb, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+console.log(mongodb)
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use("/api", profile)
 
 if (process.env.NODE_ENV == "production") {
-   app.get("*", (req, res) => {
-       res.sendFile(path.join(__dirname, "..", "heap-up", "build.html"));
-   }) 
+  //app.get('*', (req,res) => {
+    //res.sendFile(path.join(__dirname, "..", "heap-up", "build", "index.html"))
+  //})
+  app.use(express.static(path.join(__dirname, "..", "heap-up", "build")))
 }
 
 app.listen(PORT, () => {
